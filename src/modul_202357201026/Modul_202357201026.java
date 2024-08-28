49043aa6ac809d9395eb8acf335585b24f5f3275
hisyam <haritshisyam9@gmail.com>
2024-08-28 21:07:20 +0700

    Rerata

--- src/modul_202357201026/Modul_202357201026.java
+++ src/modul_202357201026/Modul_202357201026.java
@@ -16,13 +16,21 @@ public class Modul_202357201026 {
     public static void main(String[] args) {
         // TODO code application logic here
         int data [][] = {{4,6,4,2,8,4,2,10,},{4,6,4,2,8,4,2,10}};
+        double total =0;
+        double jumlahelemen =0;
         
-        for (int[] data1 : data) {
-            for (int kolom = 0; kolom < data1.length; kolom++) {
-                System.out.print(data1[kolom] + " ");
+        for (int baris= 0; baris <data.length; baris++) {
+            for (int kolom = 0; kolom < data[baris].length; kolom++) {
+                
+                System.out.print(data[baris][kolom] + " ");
+                total += data[baris][kolom];
+                jumlahelemen++;
             }
             System.out.print("\n");
         }
+        double rerata = total/jumlahelemen;
+        System.out.println("total = " + total);
+        System.out.println("rerata = " + rerata);
     }
     
 }
