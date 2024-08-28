faa7558422615ac97c7a36e9accb79dec6463f81
hisyam <haritshisyam9@gmail.com>
2024-08-28 20:43:38 +0700

    data[baris][kolom] + " "

--- src/modul_202357201026/Modul_202357201026.java
+++ src/modul_202357201026/Modul_202357201026.java
@@ -15,6 +15,14 @@ public class Modul_202357201026 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int data [][] = {{4,6,4,2,8,4,2,10,},{4,6,4,2,8,4,2,10}};
+        
+        for (int[] data1 : data) {
+            for (int kolom = 0; kolom < data1.length; kolom++) {
+                System.out.print(data1[kolom] + " ");
+            }
+            System.out.print("\n");
+        }
     }
     
 }
